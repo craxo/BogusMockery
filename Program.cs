1c15ea9fb066d9c20500ed3c56182cd5ee356eb1
craxo <38186154+craxo@users.noreply.github.com>
2024-03-01 00:14:32 +0000

    Add some bogus employees

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Employee;
+using Bogus;
+
+// Set some random full name
+var faker = new Faker();
+string randomName = faker.Name.FullName();
+Console.WriteLine(randomName);
+
+// We have a EmployeeInfo which we want to fill, lets fill hundred!
+for(int i = 0; i < 100; i++)
+{
+    var bogusEmployee = EmployeeExtensions.GetBogusEmployee();
+    Console.WriteLine("####################################");
+    Console.WriteLine($"First Name: {bogusEmployee.FirstName}");
+    Console.WriteLine($"Last Name: {bogusEmployee.LastName}");
+    Console.WriteLine($"Gender: {bogusEmployee.Gender}");
+    Console.WriteLine($"Age: {bogusEmployee.Age}");
+}
